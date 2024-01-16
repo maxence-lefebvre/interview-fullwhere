d4a96d98ca4eaa9728cd616b627f4f139ef6566c
mlefebvre-amalia <maxence@amalia.io>
2024-01-16 09:27:38 +0100

    feat: implement feedbacks graphql query

--- libs/features/feedbacks/graphql/src/lib/resolvers/feedback.resolver.ts
+++ libs/features/feedbacks/graphql/src/lib/resolvers/feedback.resolver.ts
@@ -1,14 +1,38 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import {
+  FeedbackModel,
+  FeedbackProvider,
+  FeedbackStatus,
+} from '@fullwhere/features/feedbacks/domain';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { FindOptionsWhere, LessThanOrEqual, MoreThanOrEqual } from 'typeorm';
 
 import { FeedbackRepository } from '@fullwhere/features/feedbacks/data-access';
-import { FeedbackModel } from '@fullwhere/features/feedbacks/domain';
 
 @Resolver(() => FeedbackModel)
 export class FeedbackResolver {
   public constructor(private readonly feedbackRepository: FeedbackRepository) {}
 
-  @Query(() => [FeedbackModel])
-  list(): Promise<FeedbackModel[]> {
-    return this.feedbackRepository.find();
+  @Query(() => [FeedbackModel], { name: 'feedbacks' })
+  feedbacks(
+    @Args('status', { nullable: true, type: () => FeedbackStatus })
+    status: FeedbackStatus | null = null,
+    @Args('provider', { nullable: true, type: () => FeedbackProvider })
+    provider: FeedbackProvider | null = null,
+    @Args('minNote', { nullable: true, type: () => Int })
+    minNote: number | null = null,
+    @Args('createdAfter', { nullable: true })
+    createdAfter: Date | null = null,
+    @Args('createdBefore', { nullable: true })
+    createdBefore: Date | null = null,
+  ): Promise<FeedbackModel[]> {
+    return this.feedbackRepository.find({
+      where: {
+        ...(status && { status }),
+        ...(provider && { provider }),
+        ...(minNote && { note: MoreThanOrEqual(minNote) }),
+        ...(createdAfter && { createdAt: MoreThanOrEqual(createdAfter) }),
+        ...(createdBefore && { createdAt: LessThanOrEqual(createdBefore) }),
+      } satisfies FindOptionsWhere<FeedbackModel>,
+    });
   }
 }
